cca1870765839c92fc75a81556b1e34c12867d01
N3dal <nedalxzo@gmail.com>
2022-12-19 23:29:37 +0200

    update main.py

--- main.py
+++ main.py
@@ -20,6 +20,59 @@ import sys
 import tools
 
 
+class TitleBar(QFrame):
+    """
+        Custom Title bar for the Main window;
+    """
+
+    HEIGHT = 31
+
+    STYLESHEET = """
+        background-color: #8b949e;
+        
+        border-top-left-radius: 10px;
+        border-top-right-radius:10px;
+        border-bottom-left-radius: 0px;
+        border-bottom-right-radius: 0px;
+
+    """
+
+    TITLE = "Quick-Do"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.setFixedSize(self.parent().width(), TitleBar.HEIGHT)
+
+        self.setStyleSheet(TitleBar.STYLESHEET)
+
+
+class MainFrame(QFrame):
+    """
+        The main Frame;
+    """
+    STYLESHEET = """
+        background: QLinearGradient(x1 :0, y1:0,
+                                    x2: 1, y2: 0,
+                                    stop: 0 #005C97,
+                                    stop: 1 #363795);
+                                    
+        border-top-left-radius: 0px;
+        border-top-right-radius: 0px;
+        border-bottom-left-radius: 15px;
+        border-bottom-right-radius: 15px;
+            
+        
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.setFixedSize(self.parent().width(), self.parent().height() - 50)
+
+        self.setStyleSheet(MainFrame.STYLESHEET)
+
+
 class MainWindow(QMainWindow):
     """
         the app main-window;
@@ -27,7 +80,10 @@ class MainWindow(QMainWindow):
 
     WIDTH = 350
     HEIGHT = 550
+
     STYLESHEET = """"""
+
+    OPACITY = 0.98
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -35,6 +91,21 @@ class MainWindow(QMainWindow):
         self.setFixedSize(MainWindow.WIDTH, MainWindow.HEIGHT)
 
         self.setStyleSheet(MainWindow.STYLESHEET)
+
+        self.setWindowOpacity(MainWindow.OPACITY)
+
+        self.setAttribute(Qt.WA_TranslucentBackground)
+
+        # to remove the title bar and the frame;
+        self.setWindowFlags(Qt.FramelessWindowHint)
+
+        # create the title bar;
+        self.title_bar = TitleBar(parent=self)
+        self.title_bar.move(0, 0)
+
+        # create the main Frame;
+        self.main_frame = MainFrame(parent=self)
+        self.main_frame.move(0, TitleBar.HEIGHT-5)
 
 
 def main():
